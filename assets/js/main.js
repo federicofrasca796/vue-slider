11890c58c1f1f10e7909b2f797f9f241fc7bf91d
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-29 17:06:46 +0200

    add thumbs scroll on click

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,31 +6,41 @@ const root = new Vue({
                 img: 'img/01.jpg',
                 title: 'Svezia',
                 text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                
                 status: true,
+                classActive: 'active',
             },
             {
                 img: 'img/02.jpg',
                 title: 'Svizzera',
                 text: 'Lorem ipsum.',
+                
                 status: false,
+                classActive: '',
             },
             {
                 img: 'img/03.jpg',
                 title: 'Gran Bretagna',
                 text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                
                 status: false,
+                classActive: '',
             },
             {
                 img: 'img/04.jpg',
                 title: 'Germania',
                 text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                
                 status: false,
+                classActive: '',
             },
             {
                 img: 'img/05.jpg',
                 title: 'Paradise',
                 text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                
                 status: false,
+                classActive: '',
             },
         ],
 
@@ -44,11 +54,19 @@ const root = new Vue({
         
         scrollDown(){
             if(this.counter < this.cards.length - 1){
+                //display none to previous hero img
                 this.cards[this.counter].status = false;
+                this.cards[this.counter].classActive = '';
+                
+                //increment counter
                 this.counter++
-    
+
+                //display block to current hero img
                 this.cards[this.counter].status = true;
-                console.log(this.cards);
+                this.cards[this.counter].classActive = 'active';
+                // console.log(this.cards);
+
+
             }
         }
 
