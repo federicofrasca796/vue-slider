3261d7d096c8e1d86e3b8c4edab82e578df64e4f
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-29 16:24:50 +0200

    add show hero_content w/ v-if condition

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,33 +6,46 @@ const root = new Vue({
                 img: 'img/01.jpg',
                 title: 'Svezia',
                 text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                status: true,
             },
             {
                 img: 'img/02.jpg',
                 title: 'Svizzera',
                 text: 'Lorem ipsum.',
+                status: false,
             },
             {
                 img: 'img/03.jpg',
                 title: 'Gran Bretagna',
                 text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                status: false,
             },
             {
                 img: 'img/04.jpg',
                 title: 'Germania',
                 text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                status: false,
             },
             {
                 img: 'img/05.jpg',
                 title: 'Paradise',
-                text: ' Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                status: false,
             },
         ],
 
-        counter: 1,
+        counter: 0,
     },
 
     methods: {
-        //function which increments counter here
+                scrollUp(){
+            //code here
+        },
+        
+        scrollDown(){
+
+        }
+
     }
-})
+});
+
