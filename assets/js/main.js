83d0f7ae67b6fcb5f547cb4240047f5ae77f8ca5
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-29 14:53:12 +0200

    upload assets| add html struct | Initialize VueJS

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+const root = new Vue({
+    el: '#root',
+    data: {
+        cards: [
+            {
+                img: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                img: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum.',
+            },
+            {
+                img: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                img: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            },
+            {
+                img: 'img/05.jpg',
+                title: 'Paradise',
+                text: ' Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            },
+        ],
+    }
+})
